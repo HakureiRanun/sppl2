3074f20dfa7116e0e37bb40e0473dd42c0a84ca5
ranun <hakureiranun@gmail.com>
2018-10-26 11:56:27 +0800

    init

---
+++ tutorial/spiders/minori_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy import Request
+from tutorial.items import SpplItem
+import logging
+import time
+import requests
+
+class MinoriSpider(scrapy.spiders.Spider):
+    name = "minori"
+    allowed_domains = ["http://www.mangagamer.org/"]
+    start_urls = [
+        "http://www.mangagamer.org/minori/",
+    ]
+
+    def parse(self, response):
+        sppl = SpplItem()
+        sppl["Date"] = time.strftime('%Y-%m-%d',time.localtime())
+        sppl["price"] = response.xpath('//span[@class="current"]/text()').extract()[0]
+        with open('sppl.json','ab') as f:
+            f.write(sppl)
+        yield sppl
